f8bf679157c1a3c11a97b61cfe4a970ff4c483e9
cmavelis <cmavelis@gmail.com>
2022-11-15 21:55:21 -0800

    initial commit -- vue starter with board

---
+++ src/components/BoardComponent.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import BoardPiece from "@/components/BoardPiece.vue";
+
+const squares = Array(80).fill("");
+const pieces = [
+  {
+    type: "p1",
+    position: [1, 3],
+  },
+  {
+    type: "p2",
+    position: [4, 4],
+  },
+];
+</script>
+
+<template>
+  <div class="board-wrapper">
+    <div class="board-container">
+      <div v-for="square in squares" :key="square" class="board-square"></div>
+      <BoardPiece v-for="piece in pieces" :key="piece.type" v-bind="piece" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board-wrapper {
+  --square-size: 50px;
+  position: relative;
+}
+
+.board-container {
+  display: grid;
+  grid-template-columns: repeat(10, var(--square-size));
+  grid-template-rows: repeat(8, var(--square-size));
+  border: 1px solid blanchedalmond;
+  width: fit-content;
+  margin: auto;
+}
+
+.board-square {
+  border: 1px solid blanchedalmond;
+}
+</style>
---
+++ src/components/BoardPiece.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+
+interface PieceProps {
+  type: string;
+  position: [number, number];
+}
+
+const props = defineProps<PieceProps>();
+
+const squareSize = 50;
+
+const translateX = `${(props.position[0] - 1) * squareSize}px`;
+const translateY = `${(props.position[1] - 1) * squareSize}px`;
+const styleObject = reactive({
+  transform: `translate(${translateX}, ${translateY})`,
+  width: squareSize + "px",
+  height: squareSize + "px",
+});
+</script>
+
+<template>
+  <div class="piece" :style="styleObject">{{ props.type }}</div>
+</template>
+
+<style scoped>
+.piece {
+  position: absolute;
+}
+</style>
