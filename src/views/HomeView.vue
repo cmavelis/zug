9cd0516d8b4db345e50b81a44a656adea272e474
cmavelis <cmavelis@gmail.com>
2022-11-29 00:19:09 -0500

    update type from client

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import BoardComponent from '../components/BoardComponent.vue';
 import { reactive } from 'vue';
-// @ts-ignore
 import { chessClient } from '@/game/App';
 import type { GameState } from '@/game/Game';
+import type { ClientState } from 'boardgame.io/dist/types/src/client/client';
 const gameState = reactive({ G: {} });
 
-const updateGameState = (args: { G: GameState }) => {
-  gameState.G = args.G;
+const updateGameState = (state: ClientState<{ G: GameState }>) => {
+  if (state) {
+    gameState.G = state.G;
+  } else {
+    console.error('A null game state update was recieved');
+  }
 };
 
 chessClient.client.subscribe(updateGameState);
