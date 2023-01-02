bf118e05082521a90fe5562ececd0eed65bacbea
cmavelis <cmavelis@gmail.com>
2023-01-02 00:01:46 -0800

    config for using server

--- src/game/App.ts
+++ src/game/App.ts
@@ -1,12 +1,16 @@
 import { Client } from 'boardgame.io/client';
-import { Local } from 'boardgame.io/multiplayer';
+import { SocketIO } from 'boardgame.io/multiplayer';
 import { type GameState, SimulChess } from './Game';
 import type { _ClientImpl } from 'boardgame.io/dist/types/src/client/client';
 
 export class SimulChessClient {
   client: _ClientImpl<any, { G: GameState; playerID: number }>;
   constructor(playerID: string) {
-    this.client = Client({ game: SimulChess, multiplayer: Local(), playerID });
+    this.client = Client({
+      game: SimulChess,
+      multiplayer: SocketIO({ server: 'localhost:8000' }),
+      playerID,
+    });
     this.client.start();
   }
 }
