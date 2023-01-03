b7866b115c282937c81c0c4d3d26ec013283fde1
cmavelis <cmavelis@gmail.com>
2023-01-02 16:22:43 -0800

    adding port to the origin config

--- server/main.ts
+++ server/main.ts
@@ -3,7 +3,10 @@ const { SimulChess } = require('../src/game/Game');
 
 const server = Server({
   games: [SimulChess],
-  origins: [Origins.LOCALHOST, `https://${process.env.RAILWAY_STATIC_URL}`],
+  origins: [
+    Origins.LOCALHOST,
+    `https://${process.env.RAILWAY_STATIC_URL}:${process.env.PORT}`,
+  ],
 });
 
 server.run(process.env.SERVER_PORT || 8000);
