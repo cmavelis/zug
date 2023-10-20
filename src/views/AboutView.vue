3ed18645807e89472fb6fcef5f9dfa7aa74cf272
cmavelis <cmavelis@gmail.com>
2023-10-20 15:57:54 -0700

    short "about" section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,40 @@
 <script setup lang="ts">
-import { LobbyClient } from 'boardgame.io/client';
-
-const lobbyClient = new LobbyClient({ server: 'http://localhost:8000' });
-
-lobbyClient
-  .listGames()
-  .then(console.log) // => ['chess', 'tic-tac-toe']
-  .catch(console.error);
-
-lobbyClient.listMatches('zug').then(console.log);
+const orders = [
+  'move-straight',
+  'move-diagonal',
+  'push-straight',
+  'push-diagonal',
+];
 </script>
 
 <template>
-  <div class="about">
-    <button>test</button>
-  </div>
+  <main class="about">
+    <h1>zug</h1>
+    <p>
+      "zug" is the name of the game. I got it from the first syllable of
+      "zugzwang", a German chess term meaning "compulsion to move". A
+      fundamental part of the design is that each player is required to use all
+      of their pieces each turn. I also thought the name was fitting because the
+      moves in this game are similar to chess pawns.
+    </p>
+    <br />
+    <p>
+      I decided to make this game as a stepping-stone to a bigger and more
+      complicated game that's been brewing in my head for over 10 years. I
+      thought I should start and finish a smaller game before trying to tackle
+      that one. I feel like it's interesting enough now that I'm committed to
+      making it good, not just throwaway practice. I hope you like it!
+    </p>
+    <br />
+    <h2>How to play:</h2>
+    <section class="grid">
+      <template v-for="order in orders" :key="order">
+        <p>{{ order }}</p>
+        <p>graphic here</p>
+        <p>desc here</p>
+      </template>
+    </section>
+  </main>
 </template>
 
 <style>
@@ -25,8 +45,9 @@ lobbyClient.listMatches('zug').then(console.log);
     align-items: center;
   }
 }
-button {
-  -webkit-appearance: none;
-  min-height: 2.5rem;
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 }
 </style>
