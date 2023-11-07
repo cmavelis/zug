da42c1fad1e333d453f1735897f308c8ff98bdc3
cmavelis <cmavelis@gmail.com>
2023-11-06 23:30:38 -0800

    decoded

--- server/auth.ts
+++ server/auth.ts
@@ -8,8 +8,7 @@ export const encodeToken = (payload: ZugUser): string => {
 
 export const decodeToken = (token: string) => {
   try {
-    const decoded = jwt.verify(token, process.env.AUTH_SECRET);
-    return decoded;
+    return jwt.verify(token, process.env.AUTH_SECRET);
   } catch (e) {
     console.error("Couldn't decode token", e);
     throw new Error("Couldn't decode token");
