0b8b17752f8a039091c591b4a4ce1b766c846da9
cmavelis <cmavelis@gmail.com>
2023-11-07 11:19:38 -0800

    extract login component, use on Matches page only

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
 import LobbyComponent from '@/components/LobbyComponent.vue';
-const { hostname } = window.location;
+import LoginComponent from '@/components/LoginComponent.vue';
 </script>
 
 <template>
   <main>
-    <RouterLink v-if="hostname === 'localhost'" to="/match/1"
-      >Go to Match #1</RouterLink
-    >
+    <LoginComponent />
     <LobbyComponent></LobbyComponent>
   </main>
 </template>
