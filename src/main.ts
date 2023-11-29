a1275d1fa858bb8c3807bd822515b10c2c2b91ee
cmavelis <cmavelis@gmail.com>
2023-11-29 10:25:12 -0800

    add primevue component lib

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import App from './App.vue';
+import router from './router';
 
-import "./assets/main.css";
+import './assets/main.css';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(PrimeVue);
 
-app.mount("#app");
+app.mount('#app');
