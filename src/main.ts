bee7b904411f9188fb8e0cbaaf0fc656f74e5d0c
cmavelis <cmavelis@gmail.com>
2023-12-09 12:15:54 -0800

    remove overrides

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import router from './router';
 import './assets/main.css';
 import 'primevue/resources/themes/lara-light-green/theme.css';
 import 'primeicons/primeicons.css';
-import './assets/prime-overrides.css';
 
 const app = createApp(App);
 
