2ca2be2fdfe56ab57a0a175960702ad9b32e9bd6
cmavelis <cmavelis@gmail.com>
2023-12-24 10:36:41 -0800

    setup test

---
+++ src/game/orders.test.ts
@@ -0,0 +1,10 @@
+import { test, expect } from 'vitest';
+import {
+  arrayToCoordinates,
+  coordinatesToArray,
+  isOppositeVector,
+  stripSecrets,
+} from '@/game/common';
+import { makeTestGame } from '@/game/test-utils';
+
+test('arrange orders, basic', () => {});
