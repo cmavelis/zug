17aa4b2d36bd7b77e2ba9510831ec85bff9d8be5
Cameron Avelis <cmavelis@gmail.com>
2024-04-26 22:16:21 -0700

    working "your turn" endpoint

--- server/types.ts
+++ server/types.ts
@@ -1,6 +1,10 @@
-import type { LobbyAPI } from 'boardgame.io/dist/types/src/types';
+import type {
+  ActivePlayers,
+  LobbyAPI,
+} from 'boardgame.io/dist/types/src/types';
 
 export interface EnhancedMatch extends LobbyAPI.Match {
+  activePlayers: ActivePlayers | null;
   score: { 0: number; 1: number };
   turn: number;
 }
