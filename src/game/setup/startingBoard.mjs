f8dade98941035030a651799536fb81b59b9effe
Cameron Avelis <cmavelis@gmail.com>
2024-05-02 21:40:34 -0700

    create fair boards

--- src/game/setup/startingBoard.mjs
+++ src/game/setup/startingBoard.mjs
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import  _  from 'lodash';
+
 const pieceSet = [2, 3, 4, 5];
 const badBoards = [
   '2-5-3-4, 2-3-5-4',
@@ -12,10 +15,10 @@ const badBoards = [
   '3-5-2-4, 5-4-2-3',
 ].map((i) => i.split(', ').map((j) => j.split('-').map(Number)));
 
-const reversedBadBoards = badBoards.map(board=>board.map(side=> side.toReversed()))
-console.log(badBoards)
-console.log(reversedBadBoards)
-const allBadBoards = badBoards.concat(reversedBadBoards)
+const reversedBadBoards = badBoards.map((board) =>
+  board.map((side) => side.toReversed()),
+);
+const allBadBoards = badBoards.concat(reversedBadBoards);
 
 function permuteUtil(nums, visited, permutation, result) {
   if (permutation.length === nums.length) {
@@ -41,7 +44,20 @@ function permute(nums) {
   return result;
 }
 
-const answer = permute(pieceSet);
+const linePermutations = permute(pieceSet);
+const allStartingBoards = linePermutations.flatMap((line1) => {
+  return linePermutations.map((line2) => {
+    return [line1,line2]
+  });
+});
 
-console.log(badBoards.length, 'unfair boards');
-console.log(answer.length, answer);
+
+const fairBoards = allStartingBoards.filter((board) => !allBadBoards.some(board2=> _.isEqual(board, board2)) )
+
+console.log(allStartingBoards.length, 'startingBoards');
+console.log(allBadBoards.length, 'unfair boards');
+console.log(fairBoards.length, 'fair boards');
+
+const json = JSON.stringify(fairBoards, null, 2)
+
+fs.writeFile('fair-boards.json', json, 'utf8', ()=> console.log('Written to file'));
