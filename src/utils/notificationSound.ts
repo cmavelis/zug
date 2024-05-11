a1d26beb1cabc49a909edc580607208882a56da1
Cameron Avelis <cmavelis@gmail.com>
2024-05-10 20:18:26 -0700

    change something

--- src/utils/notificationSound.ts
+++ src/utils/notificationSound.ts
@@ -4,7 +4,7 @@
 export const getNotificationSound = async (name?: string) => {
   let sound;
   if (name && ['Ben', 'bendeforest'].includes(name)) {
-    sound = await import('../assets/zug-a-zug-ah.mp3');
+    sound = await import('../assets/hbd.mp3');
   } else {
     sound = await import('../assets/zug-zug.mp3');
   }
